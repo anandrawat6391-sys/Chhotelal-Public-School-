a8047588c51e137524e0deeb8fe26e82f99fc8d6
Anand Rawat <anandrawat6391@gmail.com>
2026-03-23 15:14:16 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,17 @@
 import { Switch, Route } from "wouter";
-
-import Home from "./Home";
-import About from "./About";
-import Academics from "./Academics";
-import Admissions from "./Admissions";
-import Complaints from "./Complaints";
-import Contact from "./Contact";
-import Footer from "./Footer";
-import FoundersMessage from "./FoundersMessage";
-import Hero from "./Hero";
-import Navbar from "./Navbar";
-import News from "./News";
-import Testimonials from "./Testimonials";
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      {/* Fallback to 404 */}
-    </Switch>
-  );
-}
-
-function App() {
-  return (
+import Home from "../Home";
+import About from "../About";
+import Academics from "../Academics";
+import Admissions from "../Admissions";
+import Complaints from "../Complaints";
+import Contact from "../Contact";
+import Footer from "../Footer";
+import FoundersMessage from "../FoundersMessage";
+import Hero from "../Hero";
+import Navbar from "../Navbar";
+import News from "../News";
+import Testimonials from "../Testimonials";
+ return (
     <>
   <Router/>
 </>
