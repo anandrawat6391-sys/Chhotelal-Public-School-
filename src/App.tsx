429b683af41d1ae0f57c0cbb6784a27c8d1fe628
Anand Rawat <anandrawat6391@gmail.com>
2026-03-23 12:39:21 +0530

    Fix import paths in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { WhatsAppButton } from "@/components/ui/WhatsAppButton";
-import NotFound from "@/pages/not-found";
-import Home from "@/pages/Home";
+import { Toaster } from"./components/ui/toaster";
+import { TooltipProvider } from"./components/ui/tooltip";
+import { WhatsAppButton } from"./components/ui/WhatsAppButton";
+import NotFound from"./pages/not-found";
+import Home from"./pages/Home";
 
 function Router() {
   return (
