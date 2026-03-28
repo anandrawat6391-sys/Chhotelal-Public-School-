aefdcf92316780d41eed78d3506dab5e27a60cd6
Anand Rawat <anandrawat6391@gmail.com>
2026-03-29 00:54:33 +0530

    Refactor import paths in App.tsx
    Updated import paths to use relative paths instead of parent directory references.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { Switch, Route } from "wouter";
-import Home from "../Home";
-import About from "../About";
-import Academics from "../Academics";
-import Admissions from "../Admissions";
-import Complaints from "../Complaints";
-import Contact from "../Contact";
-import Footer from "../Footer";
-import FoundersMessage from "../FoundersMessage";
-import Hero from "../Hero";
-import Navbar from "../Navbar";
-import News from "../News";
-import Testimonials from "../Testimonials";
+import Home from "./Home";
+import About from "./About";
+import Academics from "./Academics";
+import Admissions from "./Admissions";
+import Complaints from "./Complaints";
+import Contact from "./Contact";
+import Footer from "./Footer";
+import FoundersMessage from "./FoundersMessage";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import News from "./News";
+import Testimonials from "./Testimonials";
 
 function App() {
   return (
